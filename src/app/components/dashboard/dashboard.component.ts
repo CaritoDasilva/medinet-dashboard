abdce56ec1b74f8cd57672588ed534673526e452
CaritoDasilva <caritodasilva@gmail.com>
2020-05-11 22:13:14 -0400

    se agregan datos a tarjetas

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,8 +9,21 @@ import { Productivity } from '../../models/productivity.model';
 })
 export class DashboardComponent implements OnInit {
   productivity: Productivity[];
+  outpatient: any[];
+  exams: any[];
+  surgeries: any[];
+  derivations: any[];
+  classification: any[];
+  generalTotal: number;
+
   constructor(private dataService: DashboardDataService) {
     this.productivity = [];
+    this.outpatient = [];
+    this.exams = [];
+    this.derivations = [];
+    this.classification = [];
+    this.surgeries = [];
+    this.generalTotal = 0;
   }
 
   ngOnInit(): void {
@@ -19,11 +32,42 @@ export class DashboardComponent implements OnInit {
   
   getDashboard() {
     this.dataService.getData().subscribe((data: Productivity[]) => {
-      this.productivity = data.map( (prod: Productivity) => new Productivity(prod.id, prod.sucursal, prod.anio, prod.mes, prod.tipo, 
-        prod.prevision, prod.cantidad, prod.total));
-        console.log(this.productivity)
+      console.log(data)
+      // this.productivity = data.map( (prod: Productivity) => new Productivity(prod.id, prod.sucursal, prod.anio, prod.mes, prod.tipo, 
+      //   prod.prevision, prod.cantidad, prod.total));
+      //   console.log(this.productivity)
+      if(data) {
+        data.forEach((prod: any) =>{
+          this.getProductivityByType(prod);
+          this.classification = [this.derivations, this.exams, this.surgeries, this.outpatient];
+          this.getGeneralTotal(prod.total)
+          console.log(this.classification)
+        })
+
+      }
     })
 
+  }
+
+  getProductivityByType(item) {
+    switch (item.tipo) {
+      case 'Derivaciones':
+        this.derivations.push(item);
+        break;
+      case 'Cirugías':
+        this.surgeries.push(item);
+        break;
+      case 'Exámenes':
+        this.exams.push(item);
+        break;
+      case 'Citas ambulatorias':
+        this.outpatient.push(item);
+        break;        
+    }    
+  }
+
+  getGeneralTotal(total: number) {
+    this.generalTotal = this.generalTotal + total;
   }
 
 }
--- src/app/shared/data-card/data-card.component.ts
+++ src/app/shared/data-card/data-card.component.ts
@@ -8,11 +8,58 @@ import { Productivity } from '../../models/productivity.model';
 })
 export class DataCardComponent implements OnInit {
 
-  // @Input() productivity: Productivity;
-  constructor() { }
+  @Input() data: any;
+
+  prevision: any[]
+  fonasaPrevision: any;
+  isaprePrevision: any;
+  qty: number;
+  total: number;
+  constructor() {
+    this.fonasaPrevision = {
+      type: 'Fonasa',
+      qty: 0
+    }
+    this.isaprePrevision = {
+      type: 'Isapre',
+      qty: 0
+    }
+    this.prevision = [this.fonasaPrevision, this.isaprePrevision];
+    this.qty = 0;
+    this.total = 0;
+  }
 
   ngOnInit(): void {
-    // console.log(this.productivity)
+    console.log(this.data)
+    this.dataClassification()
+  }
+
+  dataClassification() {
+    this.data.forEach(item => {
+
+      item.tipo === 'Citas ambulatorias' ? this.getPrevisonQty(item) : this.getQty(item.cantidad);
+      this.getTotalByType(item.total);
+    })
+    console.log(this.prevision)
+  }
+
+  getPrevisonQty(item) {
+    switch (item.prevision) {
+      case 'Fonasa':
+          this.fonasaPrevision.qty = this.fonasaPrevision.qty + item.cantidad;
+        break;
+      case 'Isapre':
+          this.isaprePrevision.qty = this.isaprePrevision.qty + item.cantidad;
+        break;
+    }
+  }
+
+  getQty(qty: number) {
+    this.qty = this.qty + qty; 
+  }
+
+  getTotalByType(total: number) {
+    this.total = total;
   }
 
 }
